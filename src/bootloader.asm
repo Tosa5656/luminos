d20e433a12ebe68d4ba87d5d4c800e9994853bb5
Tosa5656 <eea85670@gmail.com>
2026-02-04 10:54:12 +0400

    segment registers, print_string

--- src/bootloader.asm
+++ src/bootloader.asm
@@ -1,5 +1,36 @@
 bits 16
 org 0x7C00
 
+_start:
+    cli
+
+    xor ax, ax
+    mov ds, ax
+    mov es, ax
+    mov ss, ax
+    mov sp, 0x7C00
+
+    sti
+
+    mov si, hello_world
+    call print_string
+
+    jmp $
+
+print_string:
+    pusha
+    mov ah, 0x0E
+.print_char:
+    lodsb
+    test al, al
+    jz .print_done
+    int 0x10
+    jmp .print_char
+.print_done:
+    popa
+    ret
+
+hello_world db "Hello, World!", 0x0D, 0x0A, 0
+
 times 510 - ($ - $$) db 0
 dw 0xAA55
